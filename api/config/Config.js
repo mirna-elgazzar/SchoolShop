c31d2a2c5a68aadc7ba60980528a8e9f3ee51c49
mirna elgazzar <mernashg@gmail.com>
2018-05-26 05:52:05 +0200

    changed dburl

--- api/config/Config.js
+++ api/config/Config.js
@@ -10,8 +10,8 @@
 };
 */
 module.exports = {
-  FRONTEND_URI: process.env.FRONTEND_URI || 'http://localhost:4200/',
-  SECRET: '32876qihsdh76@&#!742(*#HG&#28702y&##@^!()(&^#))jhscbd', //used when we create and verify JSON Web Tokens
-  MONGO_URI: 'mongodb://localhost:27017/schoolshop',
-  EMAIL_REGEX: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    FRONTEND_URI: process.env.FRONTEND_URI || 'http://localhost:4200/',
+    SECRET: '32876qihsdh76@&#!742(*#HG&#28702y&##@^!()(&^#))jhscbd', //used when we create and verify JSON Web Tokens
+    MONGO_URI: 'mongodb://fatma:fatma@ds135810.mlab.com:35810/schoolshop',
+    EMAIL_REGEX: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 };
