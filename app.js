add72172a4c47d8266f385cd37b8903628af1ab0
mirna elgazzar <mernashg@gmail.com>
2018-05-31 15:08:26 +0200

    app.js fixings4

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use(function(req, res, next) {
 /*app.use(function(req, res) {
     res.status(404).json({
         err: null,
-        msg: '404 Not Found mizo',
+        msg: '404 Not Found',
         data: null
     });
 });*/
